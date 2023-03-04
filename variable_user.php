fb16ed6c09939b3de023ce6d0b849f1bbf038e3e
WahyuHaswar <126933472+WahyuHaswar@users.noreply.github.com>
2023-03-04 10:33:13 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,12 @@
+<?php 
+// definisikan variables
+$nama = 'Wahyu Haswar';
+$umur = 32 ;
+$berat = 65;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+echo "<br/>Hello $nama Apakabar";
+?>
